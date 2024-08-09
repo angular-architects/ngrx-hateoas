cd6b41cc9584eb0b4e0036510ed4f4c34c1db55b
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2024-08-09 16:00:16 +0200

    Added first test and github workflow

---
+++ libs/ngrx-hateoas/src/lib/util/helpers.spec.ts
@@ -0,0 +1,19 @@
+import { isValidHref } from "./helpers";
+
+describe('isValidHref', () => {
+
+    it('treats strings beginning with / as vaild', () => {
+        expect(isValidHref('/')).toBeTrue();
+        expect(isValidHref('/api')).toBeTrue();
+        expect(isValidHref('/api/foo')).toBeTrue();
+        expect(isValidHref('/api?foo=bar')).toBeTrue();
+    });
+
+    it('treats strings beginning with http as vaild', () => {
+        expect(isValidHref('http://domain.tld/api')).toBeTrue();
+        expect(isValidHref('https://domain.tld/api')).toBeTrue();
+        expect(isValidHref('http://domain.tld/api/foo')).toBeTrue();
+        expect(isValidHref('http://domain.tld/api?foo=bar')).toBeTrue();
+    });
+
+});
