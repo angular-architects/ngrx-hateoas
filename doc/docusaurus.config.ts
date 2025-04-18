8e174d30f840ef08d729c579c8339fa4490d2c75
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2025-04-18 20:15:10 +0200

    Url fix

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://angular-architects.github.io/ngrx-hateoas',
+  url: 'https://angular-architects.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ngrx-hateoas',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
