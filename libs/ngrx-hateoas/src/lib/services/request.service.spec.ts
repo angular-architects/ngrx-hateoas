3f1e00622b7f43de73ae67b64579c0f31a042a66
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2025-05-08 20:10:57 +0200

    fixed linter issues

--- libs/ngrx-hateoas/src/lib/services/request.service.spec.ts
+++ libs/ngrx-hateoas/src/lib/services/request.service.spec.ts
@@ -3,7 +3,6 @@ import { TestBed } from '@angular/core/testing';
 import { RequestService, WINDOW } from './request.service';
 import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
-import { ResourceLink, ResourceAction, ResourceSocket } from '../models';
 
 describe('RequestService', () => {
 
