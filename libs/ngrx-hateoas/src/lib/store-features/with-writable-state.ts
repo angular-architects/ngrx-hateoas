764c3d39eb05635e3a190c50d3d4d792924b923a
Daniel Murrmann <9040811+fancyDevelopment@users.noreply.github.com>
2025-12-04 00:40:42 +0100

    draft implementaton and usage of writable state

--- libs/ngrx-hateoas/src/lib/store-features/with-writable-state.ts
+++ libs/ngrx-hateoas/src/lib/store-features/with-writable-state.ts
@@ -1,11 +1,13 @@
-import { Signal, WritableSignal } from "@angular/core";
+import { isSignal, linkedSignal, Signal, WritableSignal } from "@angular/core";
 import { DeepSignal, signalStoreFeature, SignalStoreFeature, SignalStoreFeatureResult, StateSignals, withProps } from "@ngrx/signals";
 
 type StoreForWritableStateRoot<Input extends SignalStoreFeatureResult> = StateSignals<Input['state']>;
 
 export type WritableDeepSignal<T> = WritableSignal<T> & DeepSignal<T>;
 
-export type ObjectWithSignals = Record<string, Signal<unknown> | Record<string, Signal<unknown>>>;
+export type ObjectWithSignals = {
+    [key: string]: Signal<unknown> | ObjectWithSignals;
+}
 
 export type WritableStateSignals<State extends ObjectWithSignals> = {
         [Key in keyof State]: State[Key] extends ObjectWithSignals ? 
@@ -15,6 +17,20 @@ export type WritableStateSignals<State extends ObjectWithSignals> = {
       };
 
 type StateMapFn<T extends SignalStoreFeatureResult, R extends ObjectWithSignals> = (store: StoreForWritableStateRoot<T>) => R
+
+function toWritableStateSignals<T extends ObjectWithSignals>(input: T): WritableStateSignals<T> {
+    const result: ObjectWithSignals = {};
+    for (const key in input) {
+        const value = input[key];
+
+        if(isSignal(value)) {
+            result[key] = linkedSignal(() => value());
+        } else {
+            result[key] = toWritableStateSignals(value as ObjectWithSignals);
+        }
+    }
+    return result as WritableStateSignals<T>;
+}
 
 export function withWritableState<Input extends SignalStoreFeatureResult, R extends ObjectWithSignals>(
     stateMapFn: StateMapFn<Input, R>
@@ -28,6 +44,6 @@ export function withWritableState<Input extends SignalStoreFeatureResult, R exte
     stateMapFn: StateMapFn<Input, R>
 ) {
     return signalStoreFeature(
-        withProps(store => stateMapFn(store as unknown as StoreForWritableStateRoot<Input>))
+        withProps(store => toWritableStateSignals(stateMapFn(store as unknown as StoreForWritableStateRoot<Input>)))
     );
 }
