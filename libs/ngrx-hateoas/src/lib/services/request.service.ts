233727a690cee2775c775975b19bb376286eb044
Daniel Murrmann <9040811+danielmurrmann@users.noreply.github.com>
2025-12-13 21:28:35 +0100

    Optimized request handling in load methods (#49)
    * Implemented solution for issues #26 and #48

--- libs/ngrx-hateoas/src/lib/services/request.service.ts
+++ libs/ngrx-hateoas/src/lib/services/request.service.ts
@@ -1,7 +1,7 @@
 import { HATEOAS_ANTI_FORGERY, HATEOAS_CUSTOM_HEADERS, HATEOAS_LOGIN_REDIRECT, HATEOAS_METADATA_PROVIDER } from './../provide';
 import { Injectable, InjectionToken, inject } from "@angular/core";
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom, Observable } from 'rxjs';
 
 export const WINDOW = new InjectionToken<Window>('Global window object', { factory: () => window });
 
@@ -16,34 +16,41 @@ export class RequestService {
     private httpClient = inject(HttpClient);
 
     public async request<T>(method: 'GET' | 'PUT' | 'POST' | 'DELETE', url: string, body?: unknown): Promise<HttpResponse<T>> {
+        const response$ = this.requestWithObservable<T>(method, url, body);
+        return firstValueFrom(response$);
+    }
+
+    public requestWithObservable<T>(method: 'GET' | 'PUT' | 'POST' | 'DELETE', url: string, body?: unknown): Observable<HttpResponse<T>> {
         let headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-        if(this.customHeadersOptions) {
-            for(const key in this.customHeadersOptions.headers) {
+        if (this.customHeadersOptions) {
+            for (const key in this.customHeadersOptions.headers) {
                 headers = headers.set(key, this.customHeadersOptions.headers[key]);
             };
         }
-                        
-        if(method !== 'GET' && this.antiForgeryOptions) {
+
+        if (method !== 'GET' && this.antiForgeryOptions) {
             const xsrfToken = this.getXsrfCookie(this.antiForgeryOptions.cookieName);
 
-            if(xsrfToken) {
+            if (xsrfToken) {
                 headers = headers.set(this.antiForgeryOptions.headerName, xsrfToken);
             }
         }
 
         const bodyToSend = this.metadataProvider ? this.removeMetadata(body, this.metadataProvider.isMetadataKey) : body;
-        
-        try {
-            return await firstValueFrom(this.httpClient.request<T>(method, url, { body: bodyToSend, headers, observe: 'response' }));
-        } catch(errorResponse) {
-            if(typeof errorResponse === 'object' && errorResponse !== null && 'status' in errorResponse && errorResponse.status === 401 && this.loginRedirectOptions) {
+
+        let request = this.httpClient.request<T>(method, url, { body: bodyToSend, headers, observe: 'response' });
+
+        request = request.pipe(catchError(errorResponse => {
+            if (typeof errorResponse === 'object' && errorResponse !== null && 'status' in errorResponse && errorResponse.status === 401 && this.loginRedirectOptions) {
                 // Redirect to sign in 
                 const currentUrl = this.window.location.href;
                 this.window.location.href = `${this.loginRedirectOptions.loginUrl}?${this.loginRedirectOptions.redirectUrlParamName}=` + encodeURIComponent(currentUrl);
             }
             throw errorResponse;
-        }
+        }));
+
+        return request;
     }
 
     public removeMetadata<T>(obj: T, isMetadataFunc: (key: string) => boolean): T {
@@ -71,7 +78,7 @@ export class RequestService {
     }
 
     private getXsrfCookie(cookieName: string) {
-        const o=document.cookie.split(";").map(t=>t.trim()).filter(t=>t.startsWith(cookieName+"="));
-        return 0===o.length?null:decodeURIComponent(o[0].split("=")[1]);
+        const o = document.cookie.split(";").map(t => t.trim()).filter(t => t.startsWith(cookieName + "="));
+        return 0 === o.length ? null : decodeURIComponent(o[0].split("=")[1]);
     }
 }
